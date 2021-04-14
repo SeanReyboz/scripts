38530f899b0c6197162c4dbcfa38ba5596be7b98
hakkirito <seanreyboz@yahoo.fr>
2021-04-14 13:02:56 +0200

    added alert on low battery

--- dwm-status/battery.sh
+++ dwm-status/battery.sh
@@ -5,3 +5,6 @@ bat="BAT0"
 
 ### Print out the percentage of the battery
 [[ -r "/sys/class/power_supply/$bat/capacity" ]] && echo "$(cat /sys/class/power_supply/$bat/capacity)%" 
+
+## Alert on low battery
+[[ $(cat /sys/class/power_supply/$bat/capacity) -eq 5 ]] && notify-send -u critical "Warning" "Low battery"
