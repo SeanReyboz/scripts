db4ab10df3760e92129828ed56e511591132ebc3
hakkirito <seanreyboz@tutanota.com>
2021-05-16 12:28:01 +0200

    updated network script

--- dwm-status/network.sh
+++ dwm-status/network.sh
@@ -2,17 +2,18 @@
 ## Author: Sean Reyboz
 ## A simple script to display the current network status (offline, lan or wlan)
 
-## Eth
-eth="enp0s23"
-## Wifi
-wlan="wlp2s0"
+## pattern
+eth="enp*"
+wlan="wl*"
 
 ### Check which interface is being used, and display a message accordingly
-if [ $(cat /sys/class/net/$wlan/carrier) == "1"  ]; then
-        echo "$(nmcli -t -f active,ssid, dev wifi | grep '^yes' | cut -d ':' -f2)"
-elif [ $(cat /sys/class/net/$eth/carrier) == "1" ]; then 
-        echo "LAN Connected"
+if [ $(cat /sys/class/net/$wlan/carrier) 2>/dev/null == "1"  ]
+then
+    echo "$(nmcli -t -f active,ssid, dev wifi | grep '^yes' | cut -d ':' -f2)"
+elif [ $(cat /sys/class/net/$eth/carrier) 2>/dev/null == "1" ]
+then 
+    echo "LAN Connected"
 else
-        echo "No network"
+    echo "No network"
 fi
 
